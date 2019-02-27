4cf274efde7a60b67e6ecf8dd13f8593ee611a6d
aaljanabi <46700928+aaljanabi@users.noreply.github.com>
2019-02-27 13:21:47 -0800

    Add files via upload

---
+++ Exam.java
@@ -0,0 +1,19 @@
+import java.util.Random;
+public class Exam {
+	public static void main(String[] args) {
+		int position = 0;
+	    int max = 0;
+	    Random r = new Random();
+	    int step;
+	    
+	    while(-3 < position && position < 3) {
+	        System.out.println("position = " + position);
+	        step = r.nextBoolean() ? -1 : 1;
+	        position += step;
+	        max = Math.max(max, position);
+	    }
+	    
+	    System.out.println("position = " + position);
+	    System.out.println("max position = " + max);
+	}
+}
